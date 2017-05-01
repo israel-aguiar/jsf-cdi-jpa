4aef21b7e47291011852986dee94af67198e487d
Israel dos Santos Aguiar <israel.aguiar@gmail.com>
2017-05-01 15:11:05 -0300

    Cadastro de pessoa via arquivo XML

--- src/main/java/raiuga/sistema/usuario/controller/CadastrarPessoaController.java
+++ src/main/java/raiuga/sistema/usuario/controller/CadastrarPessoaController.java
@@ -1,8 +1,20 @@
 package raiuga.sistema.usuario.controller;
 
+import java.io.IOException;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.primefaces.model.UploadedFile;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import raiuga.sistema.model.PessoaModel;
 import raiuga.sistema.repository.PessoaRepository;
@@ -20,6 +32,8 @@ public class CadastrarPessoaController {
  
 	@Inject
 	PessoaRepository pessoaRepository;
+	
+	private UploadedFile file;
  
  
 	public PessoaModel getPessoaModel() {
@@ -30,6 +44,14 @@ public class CadastrarPessoaController {
 		this.pessoaModel = pessoaModel;
 	}
  
+	public UploadedFile getFile() {
+		return file;
+	}
+
+	public void setFile(UploadedFile file) {
+		this.file = file;
+	}
+
 	/**
 	 *SALVA UM NOVO REGISTRO VIA INPUT 
 	 */
@@ -48,4 +70,76 @@ public class CadastrarPessoaController {
  
 	}
  
+	
+	/**
+	 * REALIZANDO UPLOAD DE ARQUIVO
+	 */
+	public void uploadRegistros() {
+
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+		try {
+
+			if (this.file.getFileName().equals("")) {
+				Util.MensagemAtencao("Nenhum arquivo selecionado!");
+				return;
+			}
+
+			DocumentBuilder builder = factory.newDocumentBuilder();
+
+			Document doc = builder.parse(this.file.getInputstream());
+
+			Element element = doc.getDocumentElement();
+
+			NodeList nodes = element.getChildNodes();
+
+			for (int i = 0; i < nodes.getLength(); i++) {
+
+				Node node = nodes.item(i);
+
+				if (node.getNodeType() == Node.ELEMENT_NODE) {
+
+					Element elementPessoa = (Element) node;
+
+					// PEGANDO OS VALORES DO ARQUIVO XML
+					String nome = elementPessoa.getElementsByTagName("nome").item(0).getChildNodes().item(0)
+							.getNodeValue();
+					String sexo = elementPessoa.getElementsByTagName("sexo").item(0).getChildNodes().item(0)
+							.getNodeValue();
+					String email = elementPessoa.getElementsByTagName("email").item(0).getChildNodes().item(0)
+							.getNodeValue();
+					String endereco = elementPessoa.getElementsByTagName("endereco").item(0).getChildNodes().item(0)
+							.getNodeValue();
+
+					PessoaModel newPessoaModel = new PessoaModel();
+
+					newPessoaModel.setUsuarioModel(this.usuarioController.getUsuarioSession());
+					newPessoaModel.setEmail(email);
+					newPessoaModel.setEndereco(endereco);
+					newPessoaModel.setNome(nome);
+					newPessoaModel.setOrigemCadastro("X");
+					newPessoaModel.setSexo(sexo);
+
+					// SALVANDO UM REGISTRO QUE VEIO DO ARQUIVO XML
+					pessoaRepository.salvarNovoRegistro(newPessoaModel);
+				}
+			}
+
+			Util.MensagemInfo("Registros cadastrados com sucesso!");
+
+		} catch (ParserConfigurationException e) {
+
+			e.printStackTrace();
+
+		} catch (SAXException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
 }
